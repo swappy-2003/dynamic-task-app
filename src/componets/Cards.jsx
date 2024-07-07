d5d9fcbe4ccb049ee450f3013fcceadeef153598
swappy-2003 <dubeyswapnil.123@gmail.com>
2024-07-07 19:41:54 +0530

    add card component

---
+++ src/componets/Cards.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Cards() {
+  return (
+    <div className='w-60 h-72 bg-zinc-800/90  rounded-[20px]'></div>
+  )
+}
+
+export default Cards
--- src/componets/Foreground.jsx
+++ src/componets/Foreground.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function Foreground() {
   return (
-    <div className='foreground fixed top-0 left-0 z-[3] w-full h-full  '>\
-    <div className='w-60 h-72 bg-zinc-800/90      rounded-[20px]'></div>
+    <div className='foreground fixed top-0 left-0 z-[3] w-full h-full  '>
+    
     
     
     
