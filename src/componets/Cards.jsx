6d2df9b1fce55d4c1dd003ef360b5b97ae24ae0d
swappy-2003 <dubeyswapnil.123@gmail.com>
2024-07-07 20:01:24 +0530

    add react icon

--- src/componets/Cards.jsx
+++ src/componets/Cards.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { FaRegFileAlt } from "react-icons/fa";
+import { FaDownload } from "react-icons/fa6";
 
 function Cards() {
   return (
-    <div className='w-60 h-72 bg-zinc-800/90  rounded-[20px]'></div>
+    <div className='w-60 h-72 bg-zinc-800/90  rounded-[50px] text-white px-5 py-10 relative overflow-hidden'>
+        <FaRegFileAlt />
+       
+        <p className='text-l mt-5 font-semibold leading-tight '>Lorem ipsum dolor sit amet.</p>
+       <div className="footer absolute bottom-0   px-8  mb-5  w-full py-3 left-0">
+        <div className='flex items-center  justify-between'>
+            <h5>.4 mb</h5>
+            <span className='w-7 h-7 bg-zinc-300 rounded-full flex items-center justify-center'>
+            <FaDownload size=".7em"color='#000'/>
+            </span>
+            
+        </div>
+       </div>
+            data 
+            tag
+    </div>
   )
 }
 
--- src/componets/Foreground.jsx
+++ src/componets/Foreground.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-
+import Cards from './Cards'
 function Foreground() {
   return (
     <div className='foreground fixed top-0 left-0 z-[3] w-full h-full  '>
     
+    <Cards/>
     
     
     
