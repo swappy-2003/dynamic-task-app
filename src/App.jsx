c1902e7df222df94a52f165974cdf87720953bac
swappy-2003 <dubeyswapnil.123@gmail.com>
2024-07-07 22:49:17 +0530

    add analutics of vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Background from './componets/Background'
 import Foreground from './componets/Foreground'
+import { Analytics } from "@vercel/analytics/react"
 
 
 function App() {
   return (
      <div className='w-full h-screen bg-zinc-900   '>
     
-    
+    <Analytics/>
       <Background/>  
      <Foreground/>  
     </div>
