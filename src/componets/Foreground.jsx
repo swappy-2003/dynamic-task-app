6e62326833c029a0c91221ffa00c68a056f73a37
swappy-2003 <dubeyswapnil.123@gmail.com>
2024-07-08 16:02:55 +0530

    add more card

--- src/componets/Foreground.jsx
+++ src/componets/Foreground.jsx
@@ -25,6 +25,24 @@ function Foreground() {
       close: true,
       tag: { isOpen:true,tagTitle:"Upload",tagcolor:"green" },
     },
+    {
+      desc: "this is the background color of card.",
+      filesize: ".9mb",
+      close: true,
+      tag: { isOpen:false,tagTitle:"download Now",tagcolor:"green" },
+    },
+    {
+      desc: "this is the background color of card.",
+      filesize: ".9mb",
+      close: true,
+      tag: { isOpen:true,tagTitle:"download Now",tagcolor:"blue" },
+    },
+    {
+      desc: "this is the background color of card.",
+      filesize: ".9mb",
+      close: true,
+      tag: { isOpen:true,tagTitle:"Upload",tagcolor:"green" },
+    },
   ];
   useState();
   return (
